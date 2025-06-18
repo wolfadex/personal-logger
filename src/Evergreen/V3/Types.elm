011b93cbd9a55962873a43bbfbba05ac9d6f7567
Wolfgang Schuster <wolfadex@gmail.com>
2025-06-18 16:49:24 -0500

    Preparing for v3

---
+++ src/Evergreen/V3/Types.elm
@@ -0,0 +1,175 @@
+module Evergreen.V3.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Http
+import Lamdera
+import Time
+import Url
+
+
+type Submittable a
+    = Fresh a
+    | Submitting a
+    | Issue a String
+
+
+type Modifiable
+    = Unmodified String
+    | Modified
+        { original : String
+        , modified : String
+        }
+
+
+type alias EditableLog =
+    { date : Time.Posix
+    , title : Modifiable
+    , content : Modifiable
+    }
+
+
+type alias Log =
+    { date : Time.Posix
+    , title : String
+    , content : String
+    }
+
+
+type alias EditableLogList =
+    { timestamp : Time.Posix
+    , currentLog : Submittable EditableLog
+    , logHistory : List Log
+    , sha : String
+    }
+
+
+type alias UnloadedLog =
+    { timestamp : Time.Posix
+    , downloadUrl : String
+    , sha : String
+    }
+
+
+type alias LoadedLogs =
+    ( List EditableLogList, List UnloadedLog )
+
+
+type LogResult
+    = Unloaded
+    | Loading (Maybe LoadedLogs)
+    | Loaded LoadedLogs
+    | Failure (Maybe LoadedLogs) String
+
+
+type Field raw parsed
+    = Untouched raw
+    | Editing raw
+    | Committed raw (Result String parsed)
+
+
+type Theme
+    = NoTheme
+    | Theme_1
+    | Theme_2
+    | Theme_3
+    | Theme_4
+    | Theme_5
+    | Theme_6
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , newLog :
+        Submittable
+            { title : String
+            , content : String
+            }
+    , logs : LogResult
+    , settingsOpen : Bool
+    , owner : Field String String
+    , repo : Field String String
+    , token : Field String String
+    , theme : Theme
+    }
+
+
+type alias BackendModel =
+    { message : String
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | LoadLogs
+    | CreateLog
+    | OwnerChanged String
+    | OwnerBlurred
+    | RepoChanged String
+    | RepoBlurred
+    | TokenChanged String
+    | TokenBlurred
+    | NewLogTitleChanged String
+    | NewLogContentChanged String
+    | ExistingLogTitleChanged Time.Posix String
+    | ExistingLogContentChanged Time.Posix String
+    | SubmitExistingChange Time.Posix
+    | LoadMoreLogs
+    | UserClickedSettingsOpen
+    | UserClickedSettingsClose
+    | SetTheme Theme
+    | LocalStorageKeyReceived ( String, Maybe String )
+
+
+type alias ReadDetails =
+    { owner : String
+    , repo : String
+    }
+
+
+type alias WriteDetails =
+    { owner : String
+    , repo : String
+    , token : String
+    }
+
+
+type StorageDetails
+    = StorageRead ReadDetails
+    | StorageWrite WriteDetails
+
+
+type ToBackend
+    = TB_LoadLogs StorageDetails
+    | TB_CreateNewLog
+        WriteDetails
+        Bool
+        { title : String
+        , content : String
+        }
+    | TB_LoadMoreLogs (List UnloadedLog)
+    | TB_SubmitExistingChange WriteDetails EditableLogList
+
+
+type BackendMsg
+    = LogsLoadResponse Lamdera.SessionId (Result Http.Error LoadedLogs)
+    | MoreLogsLoadResponse Lamdera.SessionId (Result Http.Error (List EditableLogList))
+    | CreateNewLog
+        Lamdera.SessionId
+        WriteDetails
+        Bool
+        { title : String
+        , content : String
+        }
+        Time.Posix
+    | LogCreated Lamdera.SessionId Log (Result Http.Error String)
+    | StoreExistingChange Lamdera.SessionId WriteDetails EditableLogList Time.Posix
+    | LogStored Lamdera.SessionId ( ( Time.Posix, String ), EditableLog, List Log ) (Result Http.Error ())
+
+
+type ToFrontend
+    = TF_LogsLoaded (Result String LoadedLogs)
+    | TF_MoreLogsLoaded (Result String (List EditableLogList))
+    | TF_InsertLog Log (Result String String)
+    | TF_LogUpdated EditableLogList (Result String ())
